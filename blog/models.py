1aa4befc4076f28862f0801b04a4abb00bd26c57
zacraman <ecastillo72@uabc.edu.mx>
2017-12-12 03:26:54 +0000

    Cambie el HTML para la página.

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
-	author = models.ForeignKey('auth.User')
+	author = models.ForeignKey('auth.User',on_delete = models.DO_NOTHING)
 	title = models.CharField(max_length=200)
 	text = models.TextField()
 	created_date = models.DateTimeField(
